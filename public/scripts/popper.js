cc1d044ec6c3b5aa08c0aca38970c0d0ce5ce85f
rhit-brunkocl <brunkocl@rose-hulman.edu>
2022-11-03 15:46:08 -0400

    edit add entry changes

--- public/scripts/popper.js
+++ public/scripts/popper.js
@@ -38,6 +38,13 @@ for (var i = 0; i < longerTimeoutBrowsers.length; i += 1) {
   }
 }
 
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
 function microtaskDebounce(fn) {
   var called = false;
   return function () {
